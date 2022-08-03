05485ad91a372f1d44e9500337b7d0972286135c
Sergey <sergey.shin.t@gmail.com>
2022-08-03 20:55:20 +0600

    03.08

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// TODO: write your code here
+import sum from './basic';
+
+// eslint-disable-next-line no-unused-vars
+console.log('worked');
+
+// eslint-disable-next-line no-unused-vars
+console.log(sum([1, 2]));
